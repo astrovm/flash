78501bc4aa0601132429a88d66c7b8aafbb5e080
astrovm <~@4st.li>
2024-02-01 11:04:11 -0300

    remove files

--- docs/js/script.js
+++ docs/js/script.js
@@ -162,12 +162,5 @@ window.addEventListener("hashchange", () => {
 })(window.fetch);
 
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-        .register("sw.js")
-        .then((registration) => {
-            console.log("Service Worker registered with scope:", registration.scope);
-        })
-        .catch((error) => {
-            console.error("Service Worker registration failed:", error);
-        });
+    navigator.serviceWorker.register("sw.js");
 }
--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	globDirectory: "docs/",
 	globPatterns: [
-		"**/*.{ttf,css,ico,svg,mp3,wav,png,json,html,js,wasm,md,txt,swf,xml,phtml}",
+		"**/*.{ttf,css,ico,svg,mp3,wav,png,json,html,js,wasm,swf,xml,phtml}",
 	],
 	swDest: "docs/sw.js",
 	maximumFileSizeToCacheInBytes: 20000000,
